1eb89c39648dac83f3c47fdf579e5df5604fd370
pranaypatel512 <www.pranay1337.com@gmail.com>
2022-06-30 14:56:18 +0530

    :zap: bind store with ui and testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mobx/mobx.dart';
+import 'package:mobx_github_repos/github_store.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Github users Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,92 +27,155 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+  final GithubStore store = GithubStore();
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Github users Demo"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+      body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            UserInput(store),
+            ShowError(store),
+            LoadingIndicator(store),
+            RepositoryListView(store)
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )
     );
   }
 }
+
+class LoadingIndicator  extends StatelessWidget {
+  const LoadingIndicator(this.store);
+
+
+  final GithubStore store;
+
+  @override
+  Widget build(BuildContext context) => Observer(
+      builder: (_) => store.fetchReposFuture.status == FutureStatus.pending
+          ? const LinearProgressIndicator()
+          : Container());
+
+}
+
+class UserInput extends StatelessWidget {
+const UserInput(this.store, {Key? key}) : super(key: key);
+
+final GithubStore store;
+
+@override
+Widget build(BuildContext context) => Row(
+  children: <Widget>[
+    Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 8),
+        child: TextField(
+          autocorrect: false,
+          autofocus: true,
+          onSubmitted: (String user) {
+            store.setUser(user);
+            store.fetchRepositories();
+          },
+        ),
+      ),
+    ),
+    IconButton(
+      icon: const Icon(Icons.search),
+      onPressed: store.fetchRepositories,
+    )
+  ],
+);
+}
+
+class ShowError extends StatelessWidget {
+  const ShowError(this.store, {Key? key}) : super(key: key);
+
+  final GithubStore store;
+
+  @override
+  Widget build(BuildContext context) => Observer(
+      builder: (_) => store.fetchReposFuture.status == FutureStatus.rejected
+          ? Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+        const Icon(
+          Icons.warning,
+          color: Colors.deepOrange,
+        ),
+        Container(
+          width: 8,
+        ),
+        const Text(
+          'Failed to fetch repos for',
+          style: TextStyle(color: Colors.deepOrange),
+        ),
+        Text(
+          store.user,
+          style: const TextStyle(
+              color: Colors.deepOrange, fontWeight: FontWeight.bold),
+        )
+      ])
+          : Container());
+}
+
+class RepositoryListView  extends StatelessWidget {
+  const RepositoryListView(this.store);
+  final GithubStore store;
+
+  @override
+  Widget build(BuildContext context) => Expanded(child:
+  Observer(builder: (_) {
+    if(!store.hasResults){
+      return Container();
+    }
+
+    if(store.repositories.isEmpty){
+      return Row(mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        const Text("We could not find any repos for user:"),
+        Text(
+          store.user,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        )
+      ] ,
+      );
+    }
+
+    return ListView.builder(itemCount: store.repositories.length,
+        itemBuilder: (_,int index){
+            final repo = store.repositories[index];
+            return ListTile(
+              title: Row(
+                children: <Widget>[
+                  Text(
+                    repo.name,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(' (${repo.stargazersCount} ⭐️)'),
+                ],
+              ),
+              subtitle: Text(repo.description ?? ''),
+            );
+        });
+
+  })
+  );
+
+}
+
+
