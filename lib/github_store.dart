75bcc5d6a318a91046b21fec06c4fbc38e849d4c
pranaypatel512 <www.pranay1337.com@gmail.com>
2022-06-30 13:55:10 +0530

    :zap: created observable for fetching github repo details

---
+++ lib/github_store.dart
@@ -0,0 +1,31 @@
+import 'package:github/github.dart';
+import 'package:mobx/mobx.dart';
+
+part 'github_store.g.dart';
+
+class GithubStore = _GithubStore with _$GithubStore;
+
+abstract class _GithubStore with Store{
+  final GitHub client = GitHub();
+
+  static ObservableFuture<List<Repository>> emptyResponse = ObservableFuture.value([]);
+
+  @observable
+  ObservableFuture<List<Repository>> fetchReposFuture = emptyResponse;
+
+  List<Repository> repositories = [];
+
+  @observable
+  String user='';
+
+  @computed
+  bool get hasResults =>
+      fetchReposFuture != emptyResponse &&
+      fetchReposFuture.status == FutureStatus.fulfilled;
+
+  @action
+  void setUser(String userName){
+    fetchReposFuture = emptyResponse;
+    user = userName;
+  }
+}
